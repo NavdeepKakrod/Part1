d145a776873e6814f2cd5f781611dc7d7469721d
NavdeepKakrod <navdeepkakrod89@gmail.com>
2025-07-15 18:09:54 +0530

    Added Exercise 1.7

--- Unicafe/src/App.jsx
+++ Unicafe/src/App.jsx
@@ -4,8 +4,21 @@ const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [bad, setBad] = useState(0) 
+  const total = good+neutral+bad;
+  const average = (good-bad)/total;
+  const positive = (good*100)/total;
+  if(total === 0){
+    return (
+      <div>
+        <h1>Give Feedback</h1>
+        <button onClick={()=>{setGood(good+1)}}>Good</button>
+        <button onClick={()=>{setNeutral(neutral+1)}}>Neutral</button>
+        <button onClick={()=>{setBad(bad+1)}}>Bad</button>
+ 
+      </div>
+    )
+  }
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -14,8 +27,9 @@ const App = () => {
       <button onClick={()=>{setBad(bad+1)}}>Bad</button>
 
       <h1>Staistics</h1>
-      <p>Good {good} <br/> Neutral {neutral} <br/> Bad {bad}</p>
-  
+      
+     <p>Good {good} <br/> Neutral {neutral} <br/> Bad {bad} <br/> Average {average} <br/> Positive {positive} %</p>
+    
     </div>
   )
 }
